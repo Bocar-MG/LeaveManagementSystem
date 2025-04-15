89de00a3a7e098de97f073f8d0b5ed0ad01054f6
BocarMG <bocarmg23@gmail.com>
2025-04-15 16:10:57 +0000

    Ajoutez des fichiers projet.

---
+++ ManagementSystem.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Domain.Entities;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+
+    public string Department { get; set; }
+
+    public DateTime JoiningDate { get; set; }
+}
---
+++ ManagementSystem.Domain/Entities/LeaveRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Domain.Entities;
+
+public class LeaveRequest
+{
+    public Guid Id { get; set; }
+
+    public Guid EmployeeId { get; set; }
+
+    public string LeaveType { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public string Status { get; set; } 
+
+    public string Reason { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
